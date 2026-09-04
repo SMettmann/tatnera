b940c52d9f367b537ebcafa0d257c6684c849681
SMettmann <Sven.mettmann88@web.de>
2026-09-04 16:15:37 +0200

    Load coordinated tattoo palette after all design layers

--- design-pass.js
+++ design-pass.js
@@ -4,7 +4,8 @@
   const styles=[
     {id:'tatneraDesignPassCss',href:'design-pass.css?v=20260904-2'},
     {id:'tatneraTattooIdentityCss',href:'tattoo-identity.css?v=20260904-1'},
-    {id:'tatneraSidebarFullheightCss',href:'sidebar-fullheight.css?v=20260904-1'}
+    {id:'tatneraSidebarFullheightCss',href:'sidebar-fullheight.css?v=20260904-1'},
+    {id:'tatneraTattooPaletteCss',href:'tattoo-palette.css?v=20260904-1'}
   ];
 
   function ensureStyle(item){
