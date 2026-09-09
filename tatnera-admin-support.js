076930b1eb5ea413e529fc7081f4c392b45736e5
SMettmann <Sven.mettmann88@web.de>
2026-09-09 12:01:32 +0200

    Remove redundant admin support loader

--- tatnera-admin-support.js
+++
@@ -1,9 +0,0 @@
-/* TATNERA admin support/control loader */
-(function(){
-  'use strict';
-  if(document.querySelector('script[data-tatnera-admin-controls]'))return;
-  const script=document.createElement('script');
-  script.src='tatnera-admin-controls.js?v=20260909-1';
-  script.dataset.tatneraAdminControls='1';
-  document.head.appendChild(script);
-})();
